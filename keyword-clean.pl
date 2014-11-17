2b6f4be8766592c4ceba0ef6d0bb096549b9e545
Derek Moore <derek.p.moore@gmail.com>
2014-11-17 11:02:30 -0600

    rewrite clean filter using compile regexes, attempt idempotency

--- keyword-clean.pl
+++ keyword-clean.pl
@@ -1,20 +1,26 @@
 #!/usr/bin/env perl
-# $Author: $Format:%an <%ae>$ $Format:%an <%ae>$ $
-# $Date: $Format:%ai$ $Format:%ai$ $
-# $Revision: $Format:%h$ $Format:%h$ $
+# $Author: $Format:%an <%ae>$ $
+# $Date: $Format:%ai$ $
+# $Revision: $Format:%h$ $
 
-while (<>) {
-#s/\$Id[^\$]*\$/\$Id\$/; 
-s/\$Date: $Format:%ai$ $]*\$/\$Date: \$Format:%ai\$ \$/;
-s/\$Author: $Format:%an <%ae>$ $]*\$/\$Author: \$Format:%an <%ae>\$ \$/; 
-#s/\$Source[^\$]*\$/\$Source\$/; 
-#s/\$File[^\$]*\$/\$File\$/; 
-s/\$Revision: $Format:%h$ $]*\$/\$Revision: \$Format:%h\$ \$/;
-} continue {
-print;
+my %config = (
+    Date => '%ai',
+    Author => '%an <%ae>',
+    Revision => '%h'
+);
+
+my @regex = ();
+my @subst = ();
+for my $key (keys %config) {
+  push @regex, qr/\$$key.*\$/;
+  push @subst, "\$$key: \$Format:$config{$key}\$ \$";
 }
 
-#s/\$Date: $Format:%ai$ $]*\$/\$Date\$/;
-#s/\$Author: $Format:%an <%ae>$ $]*\$/\$Author\$/; 
-#s/\$Revision: $Format:%h$ $]*\$/\$Revision\$/;
+while (<>) {
+  for my $i (0..scalar keys %config) {
+    s/$regex[$i]/$subst[$i]/;
+  }
+} continue {
+  print;
+}
 
